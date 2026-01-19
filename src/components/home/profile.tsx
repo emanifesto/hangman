f0a51a19104ccc53d80ab516e2ee1f9c82ae223b
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-18 19:15:03 -0500

    old table code cleanup

--- src/components/home/profile.tsx
+++ src/components/home/profile.tsx
@@ -32,7 +32,6 @@ function User(){
 function Info(){
     return (
         <div className="flex px-5 justify-between">
-            {/* <h1 className="text-2xl w-min ml-4">Stats</h1> */}
             <div className="font-extrabold mt-6">
                 <p>Games (W/L/Total)</p>
                 <p>Score</p>
@@ -70,50 +69,6 @@ function Info(){
                     <p>{user.all_time.flawless_games}</p>
                 </div>
             </div>
-            {/* <table className="mx-auto border-spacing-x-7.5 border-spacing-y-2 border-separate">
-                <thead>
-                    <th></th>
-                    <th><p>Daily</p></th>
-                    <th><p>Weekly</p></th>
-                    <th><p>All-Time</p></th>
-                </thead>
-                <tr>
-                    <th><p>Games(W/L/Total)</p></th>
-                    <td><p>{user.daily.games_won + ' / ' + user.daily.games_lost + ' / ' + user.daily.games_total}</p></td>
-                    <td><p>{user.weekly.games_won + ' / ' + user.weekly.games_lost + ' / ' + user.weekly.games_total}</p></td>
-                    <td><p>{user.all_time.games_won + ' / ' + user.all_time.games_lost + ' / ' + user.all_time.games_total}</p></td>
-                </tr>
-                <tr>
-                    <th><p>Score</p></th>
-                     <td><p>{user.daily.score}</p></td>
-                    <td><p>{user.weekly.score}</p></td>
-                    <td><p>{user.all_time.score}</p></td>
-                </tr>
-                <tr>
-                    <th><p>Speed</p></th>
-                     <td><p>{user.daily.speed}</p></td>
-                    <td><p>{user.weekly.speed}</p></td>
-                    <td><p>{user.all_time.speed}</p></td>
-                </tr>
-                <tr>
-                    <th><p>Accuracy</p></th>
-                     <td><p>{user.daily.accuracy}</p></td>
-                    <td><p>{user.weekly.accuracy}</p></td>
-                    <td><p>{user.all_time.accuracy}</p></td>
-                </tr>
-                <tr>
-                    <th><p>Predictions</p></th>
-                     <td><p>{user.daily.predictions}</p></td>
-                    <td><p>{user.weekly.predictions}</p></td>
-                    <td><p>{user.all_time.predictions}</p></td>
-                </tr>
-                <tr>
-                    <th><p>Flawless Games</p></th>
-                     <td><p>{user.daily.flawless_games}</p></td>
-                    <td><p>{user.weekly.flawless_games}</p></td>
-                    <td><p>{user.all_time.flawless_games}</p></td>
-                </tr>
-            </table> */}
         </div>
     )
 
