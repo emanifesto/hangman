83e4bcd8940017adfd465b72b0009961474c065a
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-20 18:11:47 -0500

    this tooks me a lot longer to figure out than expected, almost 90 minutes yikes

--- src/components/game/blanks.tsx
+++ src/components/game/blanks.tsx
@@ -7,17 +7,36 @@ export default function FillInTheBlanks(){
     const blankedWord = wordToBlank(word)
     const sections:string[] = blankedWord.split(' ')
 
+    const sectionToHTML: Function = (section: string) => {
+        const letters: string[] = section.split('')
+        return(
+            <div className="flex mx-4 gap-1 w-min">
+                {letters.map((letter: string, index: number): any => {
+                    const ret: any = letter == "_" ? <Blank /> : <p>{letter}</p>
+                    return(
+                        <div key={letter+index}>
+                            {ret}
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
     return(
         <div className="border-2">
-            <p>
-                {sections[0]}
-            </p>
-            <p>
-                {sections[1]}
-            </p>
-            <p>
-                {sections[2]}
-            </p>
+            <div className="my-3 flex flex-wrap justify-center">
+                {sectionToHTML(sections[0])}
+                {sectionToHTML(sections[1])}
+                {sectionToHTML(sections[2])}
+            </div>
         </div>
+    )
+}
+
+function Blank(){
+    return(
+        <p className="border-b-2 w-4 mt-[1lh]">
+        </p>
     )
 }
