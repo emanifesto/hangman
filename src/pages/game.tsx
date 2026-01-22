6a8e92ebc1fb27b5339cd8bf056d21f3e31294f8
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-21 20:49:56 -0500

    Am I himothy chat; core of filling in word logic is complete

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,23 +1,67 @@
-import { useEffect, type KeyboardEventHandler } from 'react'
+import { useEffect, useState } from 'react'
 import FillInTheBlanks from "../components/game/blanks.tsx"
 
-export default function Game(){
-    const word: string = "Agent00's Best Stream"
-    const wordToBlank: Function = (word: string): string => {
-        return word.replace(/[a-zA-Z]/g, "_")
-    }
-    const blankedWord = wordToBlank(word)
+const word: string = "Agent00's Best Stream".toUpperCase()
+const wordToBlank: Function = (word: string): string => {
+    return word.replace(/[a-zA-Z]/g, "_")
+}
 
+const findGuessIndexes: Function = (guess: string): number[] => {
+    const indexes: number[] = []
+    for (let i = 0; i < word.length; i++){
+        if (word.charAt(i) == guess)
+            indexes.push(i)
+    }
+    return indexes
+}
+
+const evaluateGuess: Function = (guess: string, blankedWord: string): string => {
+    if (word.includes(guess)){
+        const blankedWordArray: string[] = blankedWord.split('')
+        const replacementIndexes: number[] = findGuessIndexes(guess)
+        for (const index of replacementIndexes){
+            blankedWordArray[index] = guess
+        }
+        return blankedWordArray.join('')
+    }
+    return blankedWord
+}
+
+export default function Game(){
+    const [guess, setGuess] = useState<string>('')
+    const [blankedWord, setBlankedWord] = useState<string>(wordToBlank(word))
+    console.log(guess)
+    
     useEffect(() => {
     document.addEventListener('keydown', handleKeyDown)
     return () => {
         document.removeEventListener('keydown', handleKeyDown)
     }
 
-    }, [])
+    }, [guess])
 
     const handleKeyDown = (event:KeyboardEvent): void => {
-        console.log(event.code)
+        const keyPressed: string = event.code
+        if (keyPressed.startsWith('Key')){
+            const letter: string = keyPressed[3]
+            setGuess((prev) => prev.concat(letter.toUpperCase()))
+        }
+        
+        if (keyPressed == "Space"){
+            setGuess((prev) => prev.concat(' '))
+        }
+
+        if (keyPressed == "Enter"){
+            const newBlankedWord: string = evaluateGuess(guess, blankedWord)
+
+            if (blankedWord == newBlankedWord){
+                console.log(blankedWord)
+            } else {
+                setBlankedWord(newBlankedWord)
+            }
+
+            setGuess('')
+        }
     }
 
     return(
