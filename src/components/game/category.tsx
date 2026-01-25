03de3518b10d13d3bf563285106762e0ac12e299
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-25 18:08:08 -0500

    already faster at animating

--- src/components/game/category.tsx
+++ src/components/game/category.tsx
@@ -1,8 +1,8 @@
 export default function Category(){
     return(
-        <div className="text-xl py-3 px-5 bg-blue-600/30 rounded-xl justify-items-center mx-auto min-w-fit w-[20%]">
+        <div className="text-xl py-3 px-5 bg-[#cad9fa] rounded-xl justify-items-center mx-auto min-w-fit w-[20%]">
             <p>Category</p>
-            <div className="h-0.5 w-[90%] mx-auto my-2 bg-blue-600/50"></div>
+            <div className="h-0.5 w-[90%] mx-auto my-2 bg-[#79abf2]"></div>
             <p className="max-w-[90%] text-right">Streamers</p>{/*word is going to be a linear gradient matching to its name*/}
         </div>
     )
--- src/components/game/timer.tsx
+++ src/components/game/timer.tsx
@@ -1,9 +1,11 @@
 export default function Timer({timeLeft}: {timeLeft: number}){
+    const timerAnimation: string = ""
     return(
-        <div className="place-items-end">
+        <div className="place-items-end pr-1 py-1 relative">
             <p className="w-12 p-3 rounded-[50%] bg-orange-400 font-bold">
                 {timeLeft}
             </p>
+            <span className={`animate-hourglass absolute left-0 top-0 h-full  bg-red-500 -z-1`}></span>
         </div>
     )
 }
