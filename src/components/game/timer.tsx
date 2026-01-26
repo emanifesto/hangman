02fb4fa5c609a667402c58bd4bd0c84bf01afa3b
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-25 19:55:00 -0500

    I feel a little cracked right now; added blur to timer for added depth

--- src/components/game/timer.tsx
+++ src/components/game/timer.tsx
@@ -4,7 +4,8 @@ export default function Timer({timeLeft}: {timeLeft: number}){
             <p className="w-12 p-3 rounded-[50%] bg-black/40 font-bold">
                 {timeLeft}
             </p>
-            <span className={`animate-hourglass absolute left-0 top-0 h-full bg-[#e65555] -z-1 overflow-hidden`}>
+            <span className={`animate-hourglass absolute left-0 top-0 h-full bg-[#e65555] -z-1 overflow-hidden
+                after:bg-[#7c1717] after:h-[300%] after:w-8 after:absolute after:-top-3 after:left-[98%] after:animate-flash`}>
                 <span className="h-[120%] -left-full -top-1 w-4 bg-[#7c1717] absolute -skew-30 animate-pulsate" />
                 <span className="h-[120%] -left-full -top-1 w-4 bg-[#7c1717] absolute -skew-30 [animation-delay:500ms] animate-pulsate" />
                 <span className="h-[120%] -left-full -top-1 w-4 bg-[#7c1717] absolute -skew-30 [animation-delay:1000ms] animate-pulsate" />
