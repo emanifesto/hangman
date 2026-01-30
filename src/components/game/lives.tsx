b8a58651112d174d202c6ade0d4a222844740630
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-29 23:03:20 -0500

    body parts component practically finished; just some touch ups needed

--- src/components/game/lives.tsx
+++ src/components/game/lives.tsx
@@ -4,62 +4,45 @@ export default function Lives(){
 
     return(
         <div className='bg-[#cad9fa] inset-shadow-sm inset-shadow-[#5078b4] p-4 basis-1/2 content-center place-items-center rounded-xl col-span-3 row-span-5 col-start-1'>
-            <BodyPartBox 
-                bodyPart={
-                    <div className='w-4 h-4 bg-red-400 rounded-[50%]'></div>
-                }
-                status="alive"/>
+            <BodyPartBox status="alive">
+                <img src='src/assets/Head.svg' alt='a svg of a head' className='w-6 h-auto animate-pulse'/>
+            </BodyPartBox>
             <div className='flex'>
-                <BodyPartBox 
-                    bodyPart={
-                        <div className='w-2 h-6 bg-red-400'></div>
-                    }
-                    status="alive"/>
-                <BodyPartBox 
-                    bodyPart={
-                        <div className='w-4 h-6 bg-red-400'></div>
-                    }
-                    status="alive"/>
-                <BodyPartBox 
-                    bodyPart={
-                        <div className='w-2 h-6 bg-red-400'></div>
-                    }
-                    status="alive"/>
+                <BodyPartBox status="alive">
+                    <img src='src/assets/Left Arm.svg' alt='a svg of an arm' className='w-4.5 h-auto animate-pulse'/>
+                </BodyPartBox>
+                <BodyPartBox status="dead">
+                    <img src='src/assets/Torso.svg' alt='a svg of a torso' className='w-10 h-auto animate-pulse'/>
+                </BodyPartBox>
+                <BodyPartBox status="alive">
+                    <img src='src/assets/Right Arm.svg' alt='a svg of an arm' className='w-4.5 h-auto animate-pulse'/>
+                </BodyPartBox>
             </div>
-            
-            <BodyPartBox 
-                bodyPart={
-                    <div className='w-4 h-2 bg-red-400'></div>
-                }
-                status="alive"/>
+            <BodyPartBox status="alive">
+                <img src='src/assets/Waist.svg' alt='a svg of a waist' className='w-10 h-auto animate-pulse'/>
+            </BodyPartBox>
             <div className='flex'>
-                <BodyPartBox 
-                    bodyPart={
-                        <div className='w-2 h-6 bg-red-400'></div>
-                    }
-                    status="alive"/>
-                <BodyPartBox 
-                    bodyPart={
-                        <div className='w-2 h-6 bg-red-400'></div>
-                    }
-                    status="alive"/>
+                <BodyPartBox status="alive">
+                    <img src='src/assets/Left Leg.svg' alt='a svg of a leg' className='w-4 h-auto animate-pulse'/>
+                </BodyPartBox>
+                <BodyPartBox status="alive">
+                    <img src='src/assets/Right Leg.svg' alt='a svg of a leg' className='w-4 h-auto animate-pulse'/>
+                </BodyPartBox>
             </div>
             
         </div>
     )
 }
 
-function BodyPartBox({ bodyPart, status }: {bodyPart: JSX.Element, status: string}){
-    const bgcolor = {
-        alive: " bg-green-500 ",
-        dead: " bg-red-500 "
+function BodyPartBox({ children, status }: {children: JSX.Element, status: string}){
+    const filter = {
+        alive: "after:bg-green-400/40",
+        dead: "after:bg-red-500/40"
     }[status]
 
     return( 
-        <div className="p-1.5 bg-black w-min">
-            <div className={`${bgcolor} p-4 w-min rounded-xl`}>
-                {bodyPart}
-            </div>
+        <div className={`after:absolute ${filter} after:top-0 after:left-0 after:rounded-xl after:size-full p-3 inset-ring-2 content-center bg-white rounded-xl relative inset-shadow-sm`}>
+            {children}
         </div>
     )
 }
