1c4d3527a875d41888db0697ab5d8e55aba3ea9c
emanifesto <emmanuelbabayemi37@gmail.com>
2026-01-29 22:04:14 -0500

    Keyboard and Category component optimized for bigger screen size

--- src/components/game/category.tsx
+++ src/components/game/category.tsx
@@ -1,9 +1,9 @@
 export default function Category(){
     return(
-        <div className="text-lg col-span-3 row-span-4 content-center justify-items-center py-3 px-2 bg-[#cad9fa] inset-shadow-sm inset-shadow-[#5078b4] rounded-xl basis-3/8">
-            <p>Category</p>
+        <div className="col-span-3 row-span-4 content-center justify-items-center py-3 px-2 bg-[#cad9fa] inset-shadow-sm inset-shadow-[#5078b4] rounded-xl basis-3/8">
+            <p className="text-lg lg:text-2xl">Category</p>
             <div className="h-0.5 w-[95%] mx-auto my-2 bg-[#79abf2]"></div>
-            <p className="max-w-[90%] text-center text-xl">Streamers</p>{/*word is going to be a linear gradient matching to its name*/}
+            <p className="max-w-[90%] text-center text-xl lg:text-4xl">Not That Kind Of Edible</p>{/*word is going to be a linear gradient matching to its name*/}
         </div>
     )
 }
--- src/components/game/keyboard.tsx
+++ src/components/game/keyboard.tsx
@@ -23,14 +23,14 @@ function KeyboardRow( {letters, keyPress}: {letters: string, keyPress: Function}
 }
 
 function Key( {text, keyPress}:{text: string, keyPress: Function}){
-    const keyAnimation: string = "ease-out active:translate-0.5 duration-300 "
+    const keyAnimation: string = "ease-out active:translate-0.75 duration-300 "
     return(
         <div>
             <button className="relative z-0" onClick={() => keyPress(text)}>
-                <div id={text} className={`${keyAnimation} size-12 content-center place-items-center bg-[#79abf2] rounded-md font-bold`}>
+                <div id={text} className={`${keyAnimation} size-12 lg:size-14 xl:size-18 content-center place-items-center bg-[#79abf2] rounded-md font-bold`}>
                     <p className="size-fit select-none">{text.startsWith('Key') ? text[3]: text}</p>
                 </div>
-                <div className="size-12 bg-[#5078b4] absolute top-1 left-1 rounded-md -z-1 border-2">
+                <div className="size-full  bg-[#5078b4] absolute top-1 left-1 rounded-md -z-1 border-2">
                 </div>
             </button>
         </div>
