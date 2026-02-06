c05ea25c7586a8502d8595611bfb1f8f5b797ea7
emanifesto <emmanuelbabayemi37@gmail.com>
2026-02-06 13:16:20 -0500

    this is the one

--- functions/user-login.ts
+++ functions/user-login.ts
@@ -1,10 +1,10 @@
 export const onRequestPost = async (context: any) => {
     const request = await context.request.json()
     console.log(request)
-    console.log(context.params)
-    console.log(context.data)
-    console.log(context.env.ASSETS)
-    console.log(request.full)
-    console.log(request.decoded)
+    console.log(await context.params)
+    console.log(await context.data)
+    console.log(await context.env.ASSETS)
+    console.log(JSON.stringify(request.full))
+    console.log(JSON.stringify(request.decoded))
     return new Response('Success', {status: 200})
 }
