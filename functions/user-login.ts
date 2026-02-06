df87f3ea742d03e87e8392afaab5aea8bc0df407
emanifesto <emmanuelbabayemi37@gmail.com>
2026-02-06 12:44:50 -0500

    if it is stringified it must be parsed

--- functions/user-login.ts
+++ functions/user-login.ts
@@ -1,5 +1,5 @@
 export const onRequestPost = async (context: any) => {
     console.log(context)
-    console.log(context.request)
+    console.log(await JSON.parse(context.request))
     return new Response('Success', {status: 200})
 }
