f1368ac24aaf860a11810eee69d93ee41e0ea986
emanifesto <emmanuelbabayemi37@gmail.com>
2026-02-06 14:05:48 -0500

    how does one log headers

--- functions/user-login.ts
+++ functions/user-login.ts
@@ -1,55 +1,24 @@
 export const onRequestPost = async (context: any) => {
-    const headers = context.request.headers
+    
+    const headers = new Headers(context.request.headers)
     const method = context.request.method
     const body = await context.request.json()
-    const authBody = body.response
+    const authResponse = body.response
 
-    const authHeader = decodeJWTHeader(authBody.credential)
-    const authPayload = decodeJWTPayload(authBody.credential)
-    // const authSignature = decodeJWTSignature(authBody.credential)
+    const authPayload = decodeJWTPayload(authResponse.credential)
+    const authClientId = authResponse.clientId
 
+    console.log(headers.getSetCookie)
     console.log(headers)
     console.log(method)
     console.log(body)
-    console.log(authHeader)
     console.log(authPayload)
-    // console.log(authSignature)
     return new Response('Success', {status: 200})
-}
-
-function decodeJWTHeader(token: any) {
-
-    let base64Url = token.split(".")[0];
-    let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    let jsonPayload = decodeURIComponent(
-        atob(base64)
-        .split("")
-        .map(function (c) {
-            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-    return JSON.parse(jsonPayload);
 }
 
 function decodeJWTPayload(token: any) {
 
     let base64Url = token.split(".")[1];
-    let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    let jsonPayload = decodeURIComponent(
-        atob(base64)
-        .split("")
-        .map(function (c) {
-            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-    return JSON.parse(jsonPayload);
-}
-
-function decodeJWTSignature(token: any) {
-
-    let base64Url = token.split(".")[2];
     let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
     let jsonPayload = decodeURIComponent(
         atob(base64)
