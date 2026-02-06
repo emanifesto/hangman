3c3778ca14f7308dd676f9f36e74df807ee9fb00
emanifesto <emmanuelbabayemi37@gmail.com>
2026-02-06 13:04:31 -0500

    how about now

--- functions/user-login.ts
+++ functions/user-login.ts
@@ -1,8 +1,9 @@
 export const onRequestPost = async (context: any) => {
     console.log(context)
     console.log(await context.request.json())
-    console.log(await context.request.json().full.json())
-    console.log(await context.request.json().decoded.json())
+    const request = await context.request.json()
+    console.log(request.full)
+    console.log(request.decoded)
     console.log(await context.params.json())
     console.log(await context.data.json())
     console.log(await context.env.ASSETS.json())
